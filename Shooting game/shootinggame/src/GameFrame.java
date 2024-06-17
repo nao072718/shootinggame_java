e7a9e4deb605c15d9f69235725b58e32dba3bfe3
kuroro <gm27360123@gmail.com>
2024-06-17 11:30:20 +0900

    １０－８解きました

--- Shooting game/shootinggame/src/GameFrame.java
+++ Shooting game/shootinggame/src/GameFrame.java
@@ -3,26 +3,15 @@ import java.util.Vector;
 public class GameFrame extends MyFrame{
 	public void run() {
 	 GameWorld.player=new Player(100,300,0,0);
-	 GameWorld.playBullets=new Vector<PlayerBullet>();
 	 addKeyListener(GameWorld.player);
+	 GameWorld.playBullets=new Vector<PlayerBullet>();
 	 while(true) {
 		 clear();
 		 GameWorld.player.draw(this);
 		 GameWorld.player.move();
 		 movePlayerBullets();
-		 int i=0;
-		 while(i< GameWorld.playBullets.size()) {
-			 PlayerBullet b =GameWorld.playBullets.get(i);
-			 b.draw(this);
-			 b.move();
-			 if(b.y<0) {
-				 GameWorld.playBullets.remove(i);
-			 }else {
-				  i++;
-		     }
-		 }
 		 sleep(0.03);
- }
+       }
 	}
 	public void movePlayerBullets() {
 		 int i=0;
