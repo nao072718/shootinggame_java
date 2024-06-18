24e3607be2387e0533f353af5690806dd21cf347
kuroro <gm27360123@gmail.com>
2024-06-18 10:44:26 +0900

    １１ー４解きました

--- Shooting game/shootinggame/src/PlayerBullet.java
+++ Shooting game/shootinggame/src/PlayerBullet.java
@@ -4,5 +4,10 @@ public PlayerBullet(double x,double y,double vx,double vy) {
 	super(x, y, vx, vy);
 	
 }
+public void draw(MyFrame f) {
+	f.setColor(0, 128, 0);
+		f.fillRect(x+10, y+20, 10, 30);
+	
+	}
 }
 
