4ccc7a666f77d213daeba55237687b946850ee7a
kuroro <gm27360123@gmail.com>
2024-06-24 10:41:27 +0900

    １２．２解きました

--- Shooting game/shootinggame/src/GameFrame.java
+++ Shooting game/shootinggame/src/GameFrame.java
@@ -14,7 +14,8 @@ public class GameFrame extends MyFrame{
 		 GameWorld.player.move();
 		 movePlayerBullets();
 		 moveEnemies();
-		 checkPlaerAndEnemies();
+		 checkPlayerAndEnemies();
+		 checkPlayerBulletsAndEnemies();
 		 sleep(0.03);
        }
 	}
@@ -38,7 +39,7 @@ public class GameFrame extends MyFrame{
       e.move();
 		 }
 	}
-	public void checkPlaerAndEnemies() {
+	public void checkPlayerAndEnemies() {
 		for(int i=0; i < GameWorld.enemies.size(); i++) {
 			 Enemy e =GameWorld.enemies.get(i);
 			 if(Math.abs(e.x-GameWorld.player.x)<=30&&Math.abs(e.y-GameWorld.player.y)<=30) {
@@ -47,7 +48,35 @@ public class GameFrame extends MyFrame{
 			 }
 		 }
 	}
-	
+	public void checkPlayerBulletsAndEnemies() {
+		int i=0;
+       while(i<GameWorld.playBullets.size()) {
+    	   PlayerBullet b =GameWorld.playBullets.get(i);
+    		int j= 0;
+    		int hits=0;
+    		while(j<GameWorld.enemies.size()) {
+    			Enemy e = GameWorld.enemies.get(j);
+    			 if(Math.abs(e.x-b.x)<=30&&Math.abs(e.y-b.y)<=30) {
+    				 System.out.println("あたり");
+    				 hits++;
+    				 GameWorld.enemies.remove(j);
+    			 }else {
+    			     j++;
+    			 }
+    		}
+    		if(hits>0) {
+    			GameWorld.playBullets.remove(i);
+    		}else {
+    		i++;
+    		}
+       }
+		
+		
+		
+		
+		
+		
+	}
 	
 	
 	
